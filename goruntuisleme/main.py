513fbd0f5d9317471796d819d7116715f65e3056
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2022-11-07 22:14:19 +0300

    boundingBox
    Segmente edilen elin boundingBox içerisine alınması

--- goruntuisleme/main.py
+++ goruntuisleme/main.py
@@ -1,48 +1,19 @@
 import cv2
 import mediapipe
+from cvzone.HandTrackingModule import HandDetector
+
 
 camera = cv2.VideoCapture(0) #tek kamera oldugundan sifir indexini atadik
-mpHands = mediapipe.solutions.hands##elimizde noktalar cizmek icin kullanicaz
-hands = mpHands.Hands()# el objesi olusturmus oluyoruz ve icine de elde ettigimiz frame'i gondericez
-#o da frame icindeki eli tarayacak ve noktalari gondericek
-
-
-
-mpDraw = mediapipe.solutions.drawing_utils
-## elde ettigimiz noktalari cam ustune bastirmak icin kullaniyoruz
+detector = HandDetector(detectionCon=0.8, maxHands=2)
 
 while True:##surekli frame almak icin sonsuz dongu olusturduk
 
 
     # kamerayi sectikten sonra frame'leri yakalamamiz gerekiyor
     success, img = camera.read()
-    ##read() metodu iki sey dondurur
-    # bir boolean bir de img dondurur ==> yakalanan frame'i
-    # cam bir sey yakaladiysa true, yoksa false dondurur success'e
 
-    #elde ettigimiz resmi RGB formata donusturmemiz lazim.Bizim resmimiz BGR formatinda
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)#resim, format donusumu
-
-    hlms = hands.process(imgRGB)
-    #el icindeki noktalari bize bulucak
-
-    height, width, channel = img.shape
-    #penceremin yuksekligi, genisligi, channel'ı
-    #bunu yapmamizin nedeni parmaklarimizin olculerini bulmak icin koordinat*pencere boyutu yapmak
-    if hlms.multi_hand_landmarks:
-        for handlanmarks in hlms.multi_hand_landmarks:# 21 elemanli bir array.Elde 21 nokta var cunku
-
-            for fingerNum, landmark in enumerate(handlanmarks.landmark):
-                positionX, positionY =  int(landmark.x * width), int(landmark.y * height)
-                #print(fingerNum, landmark)
-
-                #parmaklara nokta num'larini yazdirdik
-                #bunu yapmamizin amaci noktalara gore kosul yazmak ve noktalari net gormek
-                cv2.putText(img, str(fingerNum), (positionX, positionY), cv2.FONT_HERSHEY_SIMPLEX,
-                            1, (255, 255, 0), 2)
-
-            mpDraw.draw_landmarks(img, handlanmarks, mpHands.HAND_CONNECTIONS)#alacagi degerler : frame, landmarkslist, connections
-            #boylelikle ellerimiz arasindaki noktalarin cizgilerini gorebiliyor olucaz
+    hands, img = detector.findHands(img) # with draw
+    #hands = detector.findHands(img, draw=False) # no draw
 
     cv2.imshow("camera window", img)
     ## iki parametre ==> bir pencere ismi, digeri de gosterilecek nesne
